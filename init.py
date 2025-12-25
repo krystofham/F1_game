4b458fac13e96a845564c5b538e961bead50fdce
krystofham <krystof.ham@volny.cz>
2025-12-25 13:54:54 +0100

    Refactors training logic into dedicated function
    Moves the car setup and training sequence from the main flow into a standalone function to improve code organization and maintainability. Replaces duplicate driver and championship lists in favor of single definitions. Enables easier future enhancements to the training process.

--- init.py
+++ init.py
@@ -1,14 +1,14 @@
 import random
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from printing_info import pit_player, info, drivers_table
-from simulating_sectors import technical_sector_sim
-from strategy import strategy
+from printing_info import *
+from simulating_sectors import *
+from strategy import *
 from mmr2 import *
 from track import *
 from team import *
 from car import *
-from weather import wet_track, generate_weather
+from weather import *
 RANK = 0
 WETTINESS = 0
 names_free_drivers = [    "Maximilian Becker", "Santiago Cruz",   "Oliver Wright", "Hiroshi Takeda",     "Sebastian Fontaine", "Mateo Silva",     "Jonas Lindberg", "Ivan Kuznetsov",     "Lorenzo Bianchi", "Connor Mitchell",    "Rafael Ortega", "Tobias Schmidt",     "Yuto Nakamura", "Charles Lambert",     "Gabriel Costa", "Andrei Petrescu",    "Lutime Meyer", "Zhang Wei",     "Finn Gallagher", "Ricardo Santos", "Micheal Unide", "Tsu tsei chui", "Simon Lambert", "Sven Olisson", "Albert McHugh"]
@@ -72,3 +72,5 @@ def reset_race(climax, cars):
         a.last_stint_start = 0
     return lap, time_laps, SAFETY_CAR, LAPS_REMAINING, weather, forecast, cars
 
+drivers = ["Alex Storme","Matteo Blaze","Hiro Tanaka","Lukas Rennhardt","Diego Ventura","Aiden Falk","Pierre Lucien","Nilapsai Vetrovski","Riku Yamashita","Carlos Navarro","Johan Reißer","Theo Hartman","Enzo DaCosta","Sebastian Krell","Marco Falcone","Ivan Vasiliev","Tyler Quinn","Jae-Min Han","Felipe Marquez","Elias Northgate","Arjun Desai","Tomás Moreira","Leo Krüger","Mikhail Antonov","Julian Stroud","Renzo Morandi"]
+championship = ["AWS Grand Prix de Espana", "AirBNB Prague GP", "eBay Skyline Turkey GP","Java airlines Monza IBM Italy GP","HP Bulgarian GP","Python circuit Bahamas", "Ostrava Apple GP", "META China Grand Prix", "Sony Varsava Grand Prix", "LG TV Grand Prix du France", "Huawei GP SPA"]
