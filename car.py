027f3a62ff9c4f8dae9ed8dcfd17d88d54212a3d
krystofham <krystof.ham@volny.cz>
2026-04-06 19:20:14 +0200

    More readable vypocitej_points_jezdec

--- car.py
+++ car.py
@@ -210,50 +210,32 @@ class Car:
         return best_pneu
     def vypocitej_points_jezdec(self, RANK):
         if self.dnf is False:
+            dict_points = {
+                1: 50,
+                2: 45,
+                3: 40,
+                4: 35,
+                5: 30,
+                6: 25,
+                7: 22,
+                8: 20,
+                9: 18,
+                10: 15,
+                11: 12,
+                12: 10,
+                13: 9,
+                14: 8,
+                15: 7,
+                16: 6,
+                17: 5,
+                18: 4,
+                19: 3,
+                20: 2,
+                21: 1,
+                22: 1,
+                23: 1
+            }
             position = RANK.index(self.name) + 1
-            if position == 1:
-                self.points += 50
-            elif position == 2:
-                self.points += 45
-            elif position == 3:
-                self.points += 40
-            elif position == 4:
-                self.points += 35
-            elif position == 5:
-                self.points += 30
-            elif position == 6:
-                self.points += 25
-            elif position == 7:
-                self.points += 22
-            elif position == 8:
-                self.points += 20
-            elif position == 9:
-                self.points += 18
-            elif position == 10:
-                self.points += 15
-            elif position == 11:
-                self.points += 12
-            elif position == 12:
-                self.points += 10
-            elif position == 13:
-                self.points += 9
-            elif position == 14:
-                self.points += 8
-            elif position == 15:
-                self.points += 7
-            elif position == 16:
-                self.points += 6
-            elif position == 17:
-                self.points += 5
-            elif position == 18:
-                self.points += 4
-            elif position == 19:
-                self.points += 3
-            elif position == 20:
-                self.points += 2
-            elif position == 21:
-                self.points += 1
-            elif position == 22:
-                self.points += 1
-            elif position == 23:
-                self.points += 1
+            for x in range(1, 24):
+                if position == x:                    
+                    self.points += dict_points[x]
