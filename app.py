0cc43a833baa86f259bdbf4b2de60d4069b2f55b
krystoham <krystof.ham@volny.cz>
2026-04-24 19:47:12 +0200

    basic structure

--- app.py
+++ app.py
@@ -2,12 +2,40 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+"""
+data in
+    driver.json
+    teams.json
+    state.json
+"""
+with open(os.path.join(_CONFIG, "drivers.json"), encoding="utf-8") as f:
+    drivers = json.load(f)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+with open(os.path.join(_CONFIG, "teams.json"), encoding="utf-8") as f:
+    teams = json.load(f)
+
+with open(os.path.join(_CONFIG, "state.json"), encoding="utf-8") as f:
+    state = json.load(f)
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/api/get_teams")
+def read_teams():
+    return {}
+
+@app.get("/api/get_team/{team_id}")
+def get_team(team_id):
+    if team_id < 12:
+        return 403
+    else:
+        return {}
+@app.get("/api/get_players")
+def get_players():
+    return {}
+
+@app.get("/api/get_player/{player_id}")
+def get_player():
+    return {}
+
+@app.get("/api/get_state")
+def get_state():
+    return {}
