2dcc8b8a4c26fc53a24d8f780ae5af608d4f0454
krystoham <krystof.ham@volny.cz>
2026-04-27 19:19:55 +0200

    Functional endpoint for MVP

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+import os
+import json
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# React
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Povolí přístup z tvého React vývojového serveru
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 """
 data in
@@ -8,6 +22,7 @@ data in
     teams.json
     state.json
 """
+_CONFIG = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(_CONFIG, "config/drivers.json"), encoding="utf-8") as f:
     drivers = json.load(f)
 
@@ -17,25 +32,48 @@ with open(os.path.join(_CONFIG, "config/teams.json"), encoding="utf-8") as f:
 with open(os.path.join(_CONFIG, "engine/state.json"), encoding="utf-8") as f:
     state = json.load(f)
 
+class Player_team(BaseModel):
+    name: str
+    driver_1: str
+    driver_2: str
+    performance_min: float
+    performance_max: float
+
+class Team(BaseModel):
+    name: str
+    performance_max: float
+    performance_min: float
+    car_indices: list[int]
+
+class Teams(BaseModel):
+    player_team: Player_team
+    teams: list[Team]
+
+
+
 
 @app.get("/api/get_teams")
-def read_teams():
+async def read_teams():
     return teams
 
 @app.get("/api/get_team/{team_id}")
-def get_team(team_id):
-    if team_id > 12:
-        return 404
-    else:
-        return teams[team_id]
-@app.get("/api/get_players")
-def get_players():
-    return {}
+async def get_team(team_id:str):
+    try:
+        if int(team_id) > 12:
+            raise HTTPException(status_code=404, detail="Team index out of range")        
+        else:
+            return teams["teams"][int(team_id)]
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Attribut should be a number")
 
-@app.get("/api/get_player/{player_id}")
-def get_player():
-    return {}
+@app.get("/api/get_drivers")
+async def get_players():
+    return drivers
+
+@app.get("/api/get_driver/{driver_id}")
+async def get_player(driver_id:str):
+    return drivers[driver_id]
 
 @app.get("/api/get_state")
-def get_state():
-    return {}
+async def get_state():
+    return state
