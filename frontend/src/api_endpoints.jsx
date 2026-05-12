593eb147fb126f632c63ac52e04bc1624094e2f8
krystoham <krystof.ham@volny.cz>
2026-05-12 17:02:43 +0200

    first touch with react and api endpoints. more files to clean visualization.

---
+++ frontend/src/api_endpoints.jsx
@@ -0,0 +1,63 @@
+const simLap = async () => {
+  try {
+    const response = await fetch("http://127.0.0.1:8000/api/sim_lap", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log("Odpověď:", data);
+  } catch (error) {
+    console.error("Chyba:", error);
+  }
+};
+
+const initRace = async () => {
+  try {
+    const response = await fetch("http://127.0.0.1:8000/api/init_race", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log("Odpověď:", data);
+  } catch (error) {
+    console.error("Chyba:", error);
+  }
+};
+
+const postRace = async () => {
+  try {
+    const response = await fetch("http://127.0.0.1:8000/api/post_race", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log("Odpověď:", data);
+  } catch (error) {
+    console.error("Chyba:", error);
+  }
+};
+
+const postChampionship = async () => {
+  try {
+    const response = await fetch("http://127.0.0.1:8000/api/post_championship", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log("Odpověď:", data);
+  } catch (error) {
+    console.error("Chyba:", error);
+  }
+};
