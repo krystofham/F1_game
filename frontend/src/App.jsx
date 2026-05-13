ce83f3d973bfc599712b145d6b494999d47d7168
krystoham <krystof.ham@volny.cz>
2026-05-13 19:20:28 +0200

    different, modern, approach to react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { 
+  createBrowserRouter, 
+  RouterProvider, 
+  Outlet 
+} from 'react-router-dom' // Změna importů
 import './index.css'
 import { simLap, initRace, postRace, postChampionship } from './api_endpoints';
 import { Team, Player, Teams, Players } from './players_teams'
 import { Navbar, HeroSection } from './ui_elements'
 
 const isMobile = window.matchMedia("(max-width: 768px)").matches;
-
 
 export async function getFullState() {
   try {
@@ -18,19 +21,35 @@ export async function getFullState() {
   }
 }
 
+const RootLayout = () => (
+  <>
+    <Navbar />
+    <div>
+      <Outlet /> 
+    </div>
+  </>
+);
 
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true, 
+        element: <HeroSection isMobile={isMobile} />
+      },
+      {
+        path: "teams",
+        element: <Teams />
+      },
+      {
+        path: "players",
+        element: <Players />
+      }
+    ]
+  }
+]);
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <div>
-        <Routes>
-          <Route path="/" element={<HeroSection isMobile={isMobile}/>} />
-          <Route path="/teams" element={<Teams />} />
-          <Route path="/players" element={<Players />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
